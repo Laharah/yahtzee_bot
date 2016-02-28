fa6b15b9a3dc7411ce826e2e91a6641607c25c91
laharah <laharah22@gmail.com>
2016-02-28 11:57:38 -0800

    Added score function and tests
      "Flush" has been omitted, will add later

--- optimal_hand.py
+++ optimal_hand.py
@@ -1,16 +1,85 @@
 import random
 import itertools
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 random.seed(0)
 
 State = namedtuple("State", 'hand, rolls')
 #  every dice combination for re-roll is represented as a mask in 0-31 in binary
 DICE_MASKS = [[int(i) for i in '{:>05}'.format(bin(n)[2:])] for n in range(31)]
+SCORE_CATEGORIES = {
+    'One Pair',
+    'Two Pair',
+    'Three of a Kind',
+    'Straight',
+    'Full House',
+    'Four of a Kind',
+    'Five of a Kind',
+}
+
 
 def roll(hand, keep_mask=None):
-    keep_mask = (0,0,0,0,0) if not keep_mask else keep_mask
-    hand = [d for d, k in zip(hand, keep_mask) if k]
+    keep_mask = (0, 0, 0, 0, 0) if not keep_mask else keep_mask
+    hand = list(itertools.compress(hand, keep_mask))
     for d in range(5 - len(hand)):
-        hand.append(random.randrange(1,7))
+        hand.append(random.randrange(1, 7))
     return tuple(hand)
+
+
+def score(hand, category):
+    if category not in SCORE_CATEGORIES:
+        raise ValueError("Invalid Category: {}".format(category))
+
+    if category == 'One Pair':
+        c = Counter(hand)
+        if c[6] == 2:
+            return 3
+        return 2 if c.most_common()[0][1] > 1 else 0
+
+    if category == 'Two Pair':
+        c = Counter(hand)
+        try:
+            p1, p2, *_ = c.most_common()
+        except ValueError:
+            return 0
+        if p1[1] == 4 or p1[1] == p2[1] == 2:
+            return 5 if p1[0] == 6 or p2[0] == 6 else 4
+        else:
+            return 0
+
+    if category == 'Three of a Kind':
+        c = Counter(hand)
+        value, n = c.most_common()[0]
+        if n < 3:
+            return 0
+        else:
+            return 8 if value == 6 else 6
+
+    if category == 'Straight':
+        start = min(hand)
+        if all(a == b for a, b in zip(sorted(hand), range(start, start + 6))):
+            return 25
+        else:
+            return 0
+
+    if category == 'Full House':
+        c = Counter(hand)
+        house, *family = c.most_common()
+        if house[1] == 5 or (house[1] == 3 and len(family) == 1):
+            return 30 if house[0] == 6 else 25
+        else:
+            return 0
+
+    if category == 'Four of a Kind':
+        c = Counter(hand)
+        most = c.most_common()[0]
+        if most[1] >= 4:
+            return 60 if most[0] == 6 else 50
+        else:
+            return 0
+
+    if category == 'Five of a Kind':
+        if len(set(hand)) > 1:
+            return 0
+        else:
+            return 240 if hand[0] == 6 else 200
--- test_optimal_hand.py
+++ test_optimal_hand.py
@@ -1,20 +1,80 @@
 import pytest
+import sys
+
+assert sys.version_info >= (3, 5), "py3.4 and below not supported"
 import optimal_hand
 
 
 def test_state():
     """test state constructor interface"""
-    s = optimal_hand.State((1,1,1,1,1), 3)
-    assert s.hand == (1,1,1,1,1)
+    s = optimal_hand.State((1, 1, 1, 1, 1), 3)
+    assert s.hand == (1, 1, 1, 1, 1)
     assert s.rolls == 3
 
 
+@pytest.mark.xfail(sys.version_info[:2] != (3, 5), reason="different randon generator")
 def test_roll():
     optimal_hand.random.seed(0)
     assert optimal_hand.roll(
         (1, 2, 3, 5, 5),
-        keep_mask=(0, 0, 0, 1, 1)) == (5, 5, 6, 5, 3)
+        keep_mask=(0, 0, 0, 1, 1)) == (5, 5, 4, 4, 1)
     assert optimal_hand.roll(
         (1, 2, 3, 4, 5),
         keep_mask=(1, 1, 1, 1, 1)) == (1, 2, 3, 4, 5)
-    assert optimal_hand.roll((1, 2, 3, 4, 5)) == (2, 4, 3, 5, 2)
+    assert optimal_hand.roll((1, 2, 3, 4, 5)) == (3, 5, 4, 4, 3)
+
+
+def test_score_invalid_category():
+    with pytest.raises(ValueError) as e:
+        optimal_hand.score((1, 2, 3, 4, 5), 'Not A Category')
+
+
+def test_score_pair():
+    assert optimal_hand.score((2, 2, 1, 4, 5), 'One Pair') == 2
+    assert optimal_hand.score((2, 6, 1, 4, 5), 'One Pair') == 0
+    assert optimal_hand.score((2, 6, 1, 6, 5), 'One Pair') == 3
+    assert optimal_hand.score((4, 6, 1, 4, 5), 'One Pair') == 2
+
+
+def test_score_two_pair():
+    assert optimal_hand.score((2, 2, 3, 4, 4), "Two Pair") == 4
+    assert optimal_hand.score((2, 2, 3, 6, 6), "Two Pair") == 5
+    assert optimal_hand.score((2, 2, 3, 2, 2), "Two Pair") == 4
+    assert optimal_hand.score((2, 2, 3, 4, 6), "Two Pair") == 0
+    assert optimal_hand.score((6, 6, 6, 6, 6), "Two Pair") == 0
+
+
+def test_score_three_of_a_kind():
+    assert optimal_hand.score((2, 2, 3, 4, 4), "Three of a Kind") == 0
+    assert optimal_hand.score((2, 1, 4, 4, 4), "Three of a Kind") == 6
+    assert optimal_hand.score((2, 6, 6, 6, 6), "Three of a Kind") == 8
+    assert optimal_hand.score((1, 2, 3, 4, 5), "Three of a Kind") == 0
+
+
+def test_score_straight():
+    assert optimal_hand.score((1, 2, 3, 4, 5), "Straight") == 25
+    assert optimal_hand.score((1, 2, 3, 4, 4), "Straight") == 0
+    assert optimal_hand.score((2, 3, 4, 5, 6), "Straight") == 25
+
+
+def test_score_full_house():
+    assert optimal_hand.score((1, 2, 3, 4, 5), "Full House") == 0
+    assert optimal_hand.score((3, 3, 3, 5, 5), "Full House") == 25
+    assert optimal_hand.score((3, 3, 6, 6, 6), "Full House") == 30
+    assert optimal_hand.score((3, 3, 6, 6, 5), "Full House") == 0
+    assert optimal_hand.score((3, 3, 3, 3, 6), "Full House") == 0
+    assert optimal_hand.score((3, 3, 3, 3, 3), "Full House") == 25
+
+
+def test_score_four_of_a_kind():
+    assert optimal_hand.score((2, 2, 2, 2, 6), "Four of a Kind") == 50
+    assert optimal_hand.score((2, 6, 6, 6, 6), "Four of a Kind") == 60
+    assert optimal_hand.score((6, 6, 6, 6, 6), "Four of a Kind") == 60
+    assert optimal_hand.score((2, 2, 2, 2, 2), "Four of a Kind") == 50
+    assert optimal_hand.score((3, 3, 3, 6, 6), "Four of a Kind") == 0
+
+
+def test_score_five_of_a_kind():
+    assert optimal_hand.score((2, 2, 2, 2, 6), "Five of a Kind") == 0
+    assert optimal_hand.score((2, 2, 2, 2, 2), "Five of a Kind") == 200
+    assert optimal_hand.score((6, 6, 6, 6, 6), "Five of a Kind") == 240
